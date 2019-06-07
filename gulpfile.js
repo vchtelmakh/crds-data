005b0f742cefd398dafeed9a05ae44cfd2f35ef3
Sean C Davis <scdavis41@gmail.com>
2019-06-07 15:39:21 -0400

    Rename env to "prod" and clean dist directory

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,23 @@
-const { src, dest, task, watch } = require('gulp');
+const { series, src, dest, task, watch } = require('gulp');
 
+const clean = require('gulp-clean');
 const plumber = require('gulp-plumber');
+
+function cleanDist(callback) {
+  return src('dist/*').pipe(clean());
+}
 
 function copyJsonFiles(callback) {
   return src('src/**/*.json')
     .pipe(plumber())
-    .pipe(dest('dist'))
+    .pipe(dest('dist'));
 }
 
 function watchCopyJsonFiles(callback) {
-  watch(['src/**/*.json'], copyJsonFiles);
+  watch(['src/**/*.json'], series(cleanDist, copyJsonFiles));
 }
 
-task('watch', watchCopyJsonFiles)
+task('watch', watchCopyJsonFiles);
+task('clean', cleanDist);
 
-exports.default = copyJsonFiles;
+exports.default = series(cleanDist, copyJsonFiles);
