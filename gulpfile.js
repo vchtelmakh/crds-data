eb1f43f72fdd903a63429a82c5d85b6cf1c5548d
Jeff Lay <phritolay@icloud.com>
2019-06-10 15:01:53 -0400

    us17426 validate json on build
    since jsonlint doesn't give great details on the error
    prettier has been added as well

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,19 @@
 const { series, src, dest, task, watch } = require('gulp');
 
 const clean = require('gulp-clean');
+const jsonlint = require('gulp-jsonlint');
 const plumber = require('gulp-plumber');
+const prettier = require('gulp-prettier');
 
 function cleanDist(callback) {
   return src('dist/*').pipe(clean());
 }
 
 function copyFiles(callback) {
-  return src('src/**/*')
+  return src('src/**/*.json')
+    .pipe(prettier())
+    .pipe(jsonlint())
+    .pipe(jsonlint.failOnError())
     .pipe(plumber())
     .pipe(dest('dist'));
 }
@@ -17,6 +22,7 @@ function watchCopyFiles(callback) {
   watch(['src/**/*'], series(cleanDist, copyFiles));
 }
 
+task('default', series(cleanDist, copyFiles));
 task('watch', watchCopyFiles);
 task('clean', cleanDist);
 
