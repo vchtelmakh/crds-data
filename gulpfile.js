61b3f13ebc747345920e8104a8ad58e476955a95
Jeff Lay <phritolay@icloud.com>
2019-06-11 08:41:26 -0400

    us17426 validate json on build
    ensure all files are copied to dist

--- gulpfile.js
+++ gulpfile.js
@@ -9,21 +9,27 @@ function cleanDist(callback) {
   return src('dist/*').pipe(clean());
 }
 
-function copyFiles(callback) {
+function lintFiles(callback) {
   return src('src/**/*.json')
     .pipe(prettier())
     .pipe(jsonlint())
     .pipe(jsonlint.failOnError())
     .pipe(plumber())
+}
+
+function copyFiles(callback) {
+  return src('src/**/*')
+    .pipe(plumber())
     .pipe(dest('dist'));
 }
 
 function watchCopyFiles(callback) {
+  watch(['src/**/*.json'], series(lintFiles));
   watch(['src/**/*'], series(cleanDist, copyFiles));
 }
 
-task('default', series(cleanDist, copyFiles));
+task('default', series(cleanDist, lintFiles, copyFiles));
 task('watch', watchCopyFiles);
 task('clean', cleanDist);
 
-exports.default = series(cleanDist, copyFiles);
+exports.default = series(cleanDist, lintFiles, copyFiles);
