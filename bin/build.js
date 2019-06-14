657f9ea8001502c9f2fa5c0397318f4afc3266bb
Sean C Davis <scdavis41@gmail.com>
2019-06-14 10:52:55 -0400

    US17427 - Promos Content Block Add docs to build script

--- bin/build.js
+++ bin/build.js
@@ -6,19 +6,38 @@ const fs = require('fs');
 const dest = './dist';
 const src = './src';
 
+/**
+ * Merge config.json contents and current environment variables into one config
+ * object.
+ */
 const customConfig = JSON.parse(fs.readFileSync('config.json', 'utf8'));
-this.config = Object.assign({ ...process.env }, customConfig);
+const config = Object.assign({ ...process.env }, customConfig);
 
+/**
+ * Clean destination directory.
+ */
 rimraf.sync(dest);
 
+/**
+ * Step through each of the source files, process, and build them.
+ */
 glob(`${src}/**/*`, {}, (error, files) => {
   for (let file of files.filter(f => fs.lstatSync(f).isFile())) {
+    /**
+     * Read the content and pass the config object through so those variables
+     * can be translated.
+     */
     const content = fs.readFileSync(file, 'utf8');
-    const output = mustache.render(content, this.config);
-
+    const output = mustache.render(content, config);
+    /**
+     * If it's a JSON file, attempt to parse it, which will throw an error if
+     * its invalid JSON.
+     */
     const ext = file.split('.').pop();
     if (ext.toLowerCase() === 'json') JSON.parse(output);
-
+    /**
+     * Write the file to its destination, making sure the directory exists.
+     */
     const fileDest = file.replace(src, dest);
     fs.mkdirSync(fileDest.substr(0, fileDest.lastIndexOf('/')), { recursive: true });
     fs.writeFileSync(fileDest, output);
