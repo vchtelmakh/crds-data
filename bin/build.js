e4de58f608049236f8a7b12c610c766aaf23aad6
Sean C Davis <scdavis41@gmail.com>
2019-06-14 10:38:18 -0400

    US17427 - Promos Content Block Replace gulp with node build script

---
+++ bin/build.js
@@ -0,0 +1,23 @@
+const rimraf = require('rimraf');
+const glob = require('glob');
+const mustache = require('mustache');
+const fs = require('fs');
+
+const dest = './dist';
+const src = './src';
+
+const customConfig = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+this.config = Object.assign({ ...process.env }, customConfig);
+
+rimraf.sync(dest);
+
+glob(`${src}/**/*`, {}, (error, files) => {
+  for (let file of files.filter(f => fs.lstatSync(f).isFile())) {
+    const content = fs.readFileSync(file, 'utf8');
+    const output = mustache.render(content, this.config);
+
+    const fileDest = file.replace(src, dest);
+    fs.mkdirSync(fileDest.substr(0, fileDest.lastIndexOf('/')), { recursive: true });
+    fs.writeFileSync(fileDest, output);
+  }
+});
